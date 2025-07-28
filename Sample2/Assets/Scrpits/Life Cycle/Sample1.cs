a5949ededb404e14cf2139d72a5fd2d76a95cc70
mycms2007 <mycms2007@gmail.com>
2025-07-28 12:34:49 +0900

    20250728
    오전 유니티 스크립트(생명주기)

---
+++ Sample2/Assets/Scrpits/Life Cycle/Sample1.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Sample1 : MonoBehaviour
+{
+    public CustomComponent cc;
+
+    private void Awake()
+
+    
+    {
+        cc = GetComponent<CustomComponent>();
+    }
+    
+        
+    }
