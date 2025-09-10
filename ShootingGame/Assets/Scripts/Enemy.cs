286da46e6c3732bd40ba8614f6f2bc8d91b1f19a
mycms2007 <mycms2007@gmail.com>
2025-09-10 10:43:58 +0900

    list, Linkedlist
    list, linkedlist

--- ShootingGame/Assets/Scripts/Enemy.cs
+++ ShootingGame/Assets/Scripts/Enemy.cs
@@ -1,100 +1,70 @@
 ﻿using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-//플레이어와 적의 차이
-//플레이어 : 컨트롤을 사용자가 진행합니다.
-//적 : 멀티게임이 아니라면, 따로 적해진 명령에 따라 자동으로 움직이게 됩니다.
+
 public class Enemy : MonoBehaviour
 {
     public enum EnemyType
     {
-        Down, Chase //아래로 내려가는 패턴, 플레이어를 추적하는 패턴.
+        Down, Chase // 아래로 내려가는 패턴, 플레이어를 추적하는 패턴
     }
 
-    //이동속도
     public float speed = 5;
-    public EnemyType type = EnemyType.Down; //기본적으로는 아래로 내려가는 기믹만 설계
+    public EnemyType type = EnemyType.Down;
 
+    public GameObject explosionFactory;
 
-    public GameObject explosionFactory; //폭발 공장
-
-
-    //적에 대한 패턴
+    private Vector3 dir;
 
     private void Start()
     {
         PatternSetting();
-
     }
-
-
 
     void PatternSetting()
     {
+        int rand = Random.Range(0, 10);
 
-        int rand = Random.Range(0, 10); // 0~9까지의 값 중 하나의 값을 랜덤으로 가져오겠습니다.
-
-        if (rand < 3) // 0, 1, 2 전체 숫자 10개 주3개닉까 3개.
+        if (rand < 3)
         {
-            
-                GameObject target = GameObject.FindGameObjectWithTag("Player");
-                dir = target.transform.position - transform.position; //타겟위치 - 본인위치 = 방향
-                dir.Normalize(); //방향의 크기는 1로 설정합니다.
-            }
-        else
-            {
-                type = EnemyType.Chase;
-                //아래로 내려가는 기능
-                dir = Vector3.down;
-            }
-
+            GameObject target = GameObject.FindGameObjectWithTag("Player");
+            dir = target.transform.position - transform.position;
+            dir.Normalize();
         }
-
-
-
-    // Update is called once per frame
-
-    private Vector3 dir;
+        else
+        {
+            type = EnemyType.Chase;
+            dir = Vector3.down;
+        }
+    }
 
     void Update()
     {
-        //아래로 내려가는 기능
+        // 아래로 내려가는 기능
         Vector3 dir = Vector3.down;
         transform.position += dir * speed * Time.deltaTime;
-
     }
 
     private void OnDestroy()
     {
-        var stage = FindFirstObjectByType<StageManager>(); 
+        var stage = FindFirstObjectByType<StageManager>();
 
         if (stage != null)
         {
             stage.ReportEnemyKilled();
         }
     }
-    //충돌 이벤트 설계
-    //오브젝트와 오브젝트 간의 물리적인 충돌 발생 시 호출됩니다.
-    //둘 중 하나라도 Rigidbody(강체)를 가지고 있어야 처리됩니다.
-
-    //OnCollisionEnter : 충돌 발생 시 1번 호출
-    //OnCollisionStay : 충돌 유지하는 동안 호출
-    //OnCollisionExit : 충돌 발생 후 충돌 작업에서 벗어날 경우 1번 호출
-
-    //트리거도 OnTriggerXXX로 위와 같은 형태의 문법을 가지고 있습니다.
-    //2D일 경우 OnCollisionEnter2D처럼 마지막에 2D를 명시합니다.
-
-    //일반적인 물리적 충돌 Collision(실제적으로 힘에 의해 물체가 회전하거나 이동됨)
-    //IsTrigger 체크가 진행된 오브젝트와의 트리거 충돌 Trigger(충돌 여부만 체크함)
 
     private void OnCollisionEnter(Collision collision)
     {
-        //클래스명. Instance. 메소드명()으로 기능만 사용하는 것이 가능해진다.
         ScoreManager.instance.SetScore(5);
-        GameObject explosion = Instantiate(explosionFactory, transform.position, Quaternion.identity);
+        Instantiate(explosionFactory, transform.position, Quaternion.identity);
 
-        Destroy(collision.gameObject); //상대방 파괴
-        Destroy(gameObject); //자신 파괴
+        // 🔧 수정 전: 오브젝트를 Destroy() 하면서 하이어라키에서 사라짐
+        // Destroy(collision.gameObject); // 상대방(총알) 삭제
+        // Destroy(gameObject);           // 나 자신 삭제
+
+        // 🔧 수정 후: SetActive(false) 로 하이어라키에는 남기되 기능 비활성화
+        collision.gameObject.SetActive(false); // 총알 비활성화
+        gameObject.SetActive(false);           // 적기 비활성화 (하이어라키엔 남음)
     }
-
-
 }
---
+++ list,linkedlist.cs
@@ -0,0 +1,211 @@
+﻿using System.Text;
+class MyList<T>
+{
+    //리스트를 구현하라는 말을 들으면 아래의 내용들을 칠 수 있도록 만들어 놓겠습니다.
+
+    private T[] _items;
+    private int _count;
+    private const int DefaultCapacity = 4;
+
+    public MyList()
+    {
+        _items = new T[DefaultCapacity];
+        _count = 0;
+    }
+
+    public int Count => _count;
+
+    public void Add(T item)
+    {
+        if (_count == _items.Length)
+            Resize();
+        _items[_count++] = item;
+    }
+
+    public void Insert(int index, T item)
+    {
+        if (index < 0 || index > _count)
+            throw new ArgumentOutOfRangeException();
+
+        if (_count == _items.Length)
+            Resize();
+
+        for (int i = _count; i > index; i--)
+        {
+            _items[i] = _items[i - 1];
+        }
+
+        _items[index] = item;
+        _count++;
+    }
+
+    public void RemoveAt(int index)
+    {
+        if (index < 0 || index >= _count)
+            throw new ArgumentOutOfRangeException();
+
+        for (int i = index; i < _count - 1; i++)
+        {
+            _items[i] = _items[i + 1];
+        }
+
+        _count--;
+        _items[_count] = default(T);
+    }
+
+    public T Get(int index)
+    {
+        if (index < 0 || index >= _count)
+            throw new ArgumentOutOfRangeException();
+
+        return _items[index];
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("[");
+        for (int i = 0; i < _count; i++)
+        {
+            sb.Append(_items[i]);
+            if (i < _count - 1)
+                sb.Append(", ");
+        }
+        sb.Append("]");
+        return sb.ToString();
+    }
+
+    private void Resize()
+    {
+        int newSize = _items.Length == 0 ? DefaultCapacity : _items.Length * 2;
+        T[] newArray = new T[newSize];
+        for (int i = 0; i < _count; i++)
+        {
+            newArray[i] = _items[i];
+        }
+        _items = newArray;
+    }
+}
+
+
+class MyLinkedList<T>
+{
+    private class Node
+    {
+        public T Data;
+        public Node Next;
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    private Node _head;
+    private int _count;
+
+
+
+    //링크드 리스트를 구현하라는 요구를 들으면 아래의 코드를 쳐낼 수 있도록 만들어놓겠습니다.
+    public MyLinkedList()
+    {
+        _head = null;
+        _count = 0;
+    }
+
+    public int Count => _count;
+
+    public void AddLast(T item)
+    {
+        Node newNode = new Node(item);
+        if (_head == null)
+        {
+            _head = newNode;
+        }
+        else
+        {
+            Node current = _head;
+            while (current.Next != null)
+                current = current.Next;
+            current.Next = newNode;
+        }
+        _count++;
+    }
+
+    public void Insert(int index, T item)
+    {
+        if (index < 0 || index > _count)
+            throw new ArgumentOutOfRangeException();
+
+        Node newNode = new Node(item);
+
+        if (index == 0)
+        {
+            newNode.Next = _head;
+            _head = newNode;
+        }
+        else
+        {
+            Node prev = _head;
+            for (int i = 0; i < index - 1; i++)
+                prev = prev.Next;
+
+            newNode.Next = prev.Next;
+            prev.Next = newNode;
+        }
+
+        _count++;
+    }
+
+    public void RemoveAt(int index)
+    {
+        if (index < 0 || index >= _count)
+            throw new ArgumentOutOfRangeException();
+
+        if (index == 0)
+        {
+            _head = _head.Next;
+        }
+        else
+        {
+            Node prev = _head;
+            for (int i = 0; i < index - 1; i++)
+                prev = prev.Next;
+
+            prev.Next = prev.Next.Next;
+        }
+
+        _count--;
+    }
+
+    public T Get(int index)
+    {
+        if (index < 0 || index >= _count)
+            throw new ArgumentOutOfRangeException();
+
+        Node current = _head;
+        for (int i = 0; i < index; i++)
+            current = current.Next;
+
+        return current.Data;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("[");
+
+        Node current = _head;
+        while (current != null)
+        {
+            sb.Append(current.Data);
+            if (current.Next != null)
+                sb.Append(", ");
+            current = current.Next;
+        }
+
+        sb.Append("]");
+        return sb.ToString();
+    }
+}
