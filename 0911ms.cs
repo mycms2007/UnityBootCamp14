cbb3192ab6e19a2e439475dbbec2e77362b7317b
mycms2007 <mycms2007@gmail.com>
2025-09-11 09:44:20 +0900

    0911ms
    Swap

---
+++ 0911ms.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp28
+{
+    internal class Class2
+    {
+        static void Main()
+        {
+            int x = 10;
+            int y = 20;
+            Swap(ref x, ref y);
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+          int temp = a;
+            a = b;
+            b = temp;
+               
+        }
+    }
+}
