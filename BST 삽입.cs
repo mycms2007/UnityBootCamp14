2df2c21b11ad56f807e1e2f1fa13b091a862057d
mycms2007 <mycms2007@gmail.com>
2025-09-17 14:33:30 +0900

    Create BST 삽입.cs

---
+++ BST 삽입.cs
@@ -0,0 +1,13 @@
+﻿
+private BstNode InsertRec(BstNode node, int key)
+{
+    if (node == null)
+        return new BstNode(key);
+
+    if (key < node.Key)
+        node.Left = InsertRec(node.Left, key);
+    else if (key > node.Key)
+        node.Right = InsertRec(node.Right, key);
+
+    return node;
+}
